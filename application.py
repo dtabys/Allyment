a7dc203f1f14cc825fb2f0e7f4a555558946e967
Dhruva Lokegaonkar <dhruva@lokegaonkar.in>
2020-02-22 12:42:45 -0700

    Finised /login
    - Removed flask_session dependency. It has been merged into flask
    - /login now check credentials, and handles sessions

--- application.py
+++ application.py
@@ -1,9 +1,10 @@
 from flask import Flask, request, session
 import sqlite3
+import random
+import string
 from sqlite3 import Error
 import json
-from database import init_tables
-from flask_session import Session
+from database import init_tables, check_login
 
 app = Flask(__name__)
 
@@ -11,7 +12,8 @@ database = sqlite3.connect("data/database.db")
 database.row_factory = sqlite3.Row
 
 SESSION_TYPE = 'memcached'
-Session(app)
+app.secret_key = "TEST SECRET KEY CHANGE BEFORE DEPLOYMENT"
+#session.init_app(app)
 
 @app.route("/")
 def index():
@@ -40,26 +42,39 @@ def register():
 
 @app.route("/login", methods=["POST"])
 def login():
-    response = {}
+    with sqlite3.connect("data/database.db") as database:
+        response = {}
+        cursor = database.cursor()
+        session.clear()
 
-    if (request.is_json):
-        content = request.get_json()
-        if (content["username"] and content["password"]):
-            # TODO
-            pass
+        if (request.is_json):
+            content = request.get_json()
+            if (content["username"] and content["password"]):
+                accountId = check_login(cursor, content["username"], content["password"])
+                if(accountId is None):
+                    response["status"] = "unauthorized"
+                    response["reason"] = "username or password incorrect"
+                else:
+                    session["loggedin"] = True
+                    session["accId"] = accountId
+                    session["username"] = username
+            else:
+                response["status"] = "error"
+                response["reason"] = "missing username or password"
         else:
             response["status"] = "error"
-            response["reason"] = "missing username or password"
-    else:
-        response["status"] = "error"
-        response["reason"] = "Invalid JSON"
+            response["reason"] = "Invalid JSON"
 
-    return json.dumps(response)
+        database.commit()
+        cursor.close()
+        return json.dumps(response)
 
 
 if __name__ == '__main__':
-    dbcur = database.cursor()
-    init_tables(dbcur)
-    database.commit()
-    dbcur.close()
-    app.run("127.0.0.1", "8000")
+    with sqlite3.connect("data/database.db") as database:
+        dbcur = database.cursor()
+        init_tables(dbcur)
+        database.commit()
+        dbcur.close()
+        database.close()
+        app.run("127.0.0.1", "8000")
