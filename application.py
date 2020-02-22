a3b2439b4d6ca82d388e47969e2ba211c2415bae
Dhruva Lokegaonkar <dhruva@lokegaonkar.in>
2020-02-21 22:23:19 -0700

    Added template for application.py

---
+++ application.py
@@ -0,0 +1,24 @@
+from flask import Flask, request
+import sqlite3
+
+app = Flask(__name__)
+
+database = sqlite3.connect("data/database.db")
+database.row_factory = sqlite3.Row
+dbcur = database.cursor()
+
+@app.route("/")
+def index():
+    pass
+
+@app.route("/register", methods=["POST"])
+def register():
+    if request.method == "POST":
+        name = request.form.get("name") if request.form.get("name") else False
+        password = request.form.get("password") if request.form.get("password") else False
+        pass
+    else
+        pass
+
+if __name__ == '__main__':
+    app.run("127.0.0.1", "8000")
